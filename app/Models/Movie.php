07a0888fd416be47bc5057305bf9e517f87d9829
Denis Santuryan <denissanturyan@gmail.com>
2022-11-10 13:44:59 +0400

    feat: movie and quote models

---
+++ app/Models/Movie.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Translatable\HasTranslations;
+
+class Movie extends Model
+{
+	use HasFactory;
+
+	use HasTranslations;
+
+	public array $translatable = ['title'];
+
+	public function author()
+	{
+		return $this->belongsTo(User::class, 'user_id');
+	}
+
+	public function quotes()
+	{
+		return $this->hasMany(Quote::class);
+	}
+}
