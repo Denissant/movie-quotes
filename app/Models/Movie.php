adc5ec1a0c4a76996d071a9e31f49125c43bcb83
Denis Santuryan <denissanturyan@gmail.com>
2022-11-12 21:32:52 +0400

    feat: search movies and quotes

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Translatable\HasTranslations;
@@ -15,6 +16,16 @@ class Movie extends Model
 	protected $guarded = [];
 
 	public array $translatable = ['title'];
+
+	public function scopeFilter(Builder $query, $searchQuery)
+	{
+		if ($searchQuery)
+		{
+			$query->where(
+				fn ($query) => $query->where('title', 'like', '%' . $searchQuery . '%')
+			);
+		}
+	}
 
 	public function author()
 	{
