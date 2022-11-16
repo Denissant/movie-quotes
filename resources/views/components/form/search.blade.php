bc26d9378285f54f2d95d220e69e0786913a691f
Denis Santuryan <denissanturyan@gmail.com>
2022-11-16 15:36:39 +0400

    refactor: extract search svg to components

--- resources/views/components/form/search.blade.php
+++ resources/views/components/form/search.blade.php
@@ -5,11 +5,7 @@
                placeholder="{{ __('Search...') }}">
         <button class="flex items-center justify-center px-4 border-neutral-300 border-y border-r rounded-r-md bg-indigo-500 hover:bg-indigo-700"
                 type="submit">
-            <svg class="w-6 h-6 text-gray-600" fill="white" xmlns="http://www.w3.org/2000/svg"
-                 viewBox="0 0 24 24">
-                <path
-                    d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
-            </svg>
+            <x-svg.search />
         </button>
     </div>
 </form>
